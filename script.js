aa0418d3684ba461a9196185fe6aa1339e6870de
John Wells <molekewl2017@gmail.com>
2021-05-08 16:15:19 -0700

    menu fade animation

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+const nav = document.querySelector('.nav');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -38,8 +42,9 @@ btnScrollTo.addEventListener('click', (e) => {
   const s1coords = section1.getBoundingClientRect();
   console.log(s1coords);
   console.log(e.target.getBoundingClientRect());
-  console.log('Current Scroll', window.pageXOffset, window.pageYOffset)
-  console.log('height/width viewport', 
+  console.log('Current Scroll', window.pageXOffset, window.pageYOffset);
+  console.log(
+    'height/width viewport',
     document.documentElement.clientHeight,
     document.documentElement.clientWidth
   );
@@ -56,7 +61,7 @@ btnScrollTo.addEventListener('click', (e) => {
   });
 
   // oldschool versions above. below works into new browsers
-  section1.scrollIntoView({behavior: 'smooth'})
+  section1.scrollIntoView({ behavior: 'smooth' });
 });
 
 // Page Navigation
@@ -72,36 +77,55 @@ btnScrollTo.addEventListener('click', (e) => {
 // });
 
 // Use event delegation (because of bubble up) Adding instead to parent
-document.querySelector('.nav__links').addEventListener('click', function(e) {
+document.querySelector('.nav__links').addEventListener('click', function (e) {
   if (e.target.classList.contains('nav__link')) {
     e.preventDefault();
     const id = e.target.getAttribute('href');
     document.querySelector(id).scrollIntoView({
       behavior: 'smooth'
     });
-    console.log('LINK')
+    console.log('LINK');
   }
 });
 
 // Tabbed Component
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
-
-tabsContainer.addEventListener('click', function(e) {
+tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
-  console.log(clicked)
+  console.log(clicked);
   // Guard clause (clicking outside of the buttons causes an error so this will prevent the last line from executing and stop the error and returns null instead)
-  if(!clicked) return;
+  if (!clicked) return;
 
   // Remove active classes
-  tabs.forEach(t => t.classList.remove('operations__tab--active'));
-  tabsContent.forEach(t => t.classList.remove('operations__content--active'));
+  tabs.forEach((t) => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach((t) => t.classList.remove('operations__content--active'));
 
   // Active tab
   clicked.classList.add('operations__tab--active');
 
   // Active content area
-  console.log(clicked.dataset.tab)
-  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active')
+  console.log(clicked.dataset.tab);
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
+// Menu fade animation
+const handleHover = function(e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+}
+
+// Passing "argument" into handler. In an event handler if you want to pass more than one argument you must use 'this' keyword since events only use 1 argument. if its passing more than one you can use an array or object
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+
+nav.addEventListener('mouseout', function (e) {
+  handleHover(e, 1)
 });

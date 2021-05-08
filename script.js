617f9c99547addf0bb4031f926b4a862cfa8a24e
John Wells <molekewl2017@gmail.com>
2021-05-08 16:48:23 -0700

    sticky navigation using Intersection Observer API

--- script.js
+++ script.js
@@ -110,22 +110,65 @@ tabsContainer.addEventListener('click', function (e) {
 });
 
 // Menu fade animation
-const handleHover = function(e) {
+const handleHover = function (e) {
   if (e.target.classList.contains('nav__link')) {
     const link = e.target;
     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
     const logo = link.closest('.nav').querySelector('img');
 
-    siblings.forEach(el => {
+    siblings.forEach((el) => {
       if (el !== link) el.style.opacity = this;
     });
     logo.style.opacity = this;
   }
-}
+};
 
 // Passing "argument" into handler. In an event handler if you want to pass more than one argument you must use 'this' keyword since events only use 1 argument. if its passing more than one you can use an array or object
 nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
 
-nav.addEventListener('mouseout', function (e) {
-  handleHover(e, 1)
+// Sticky Navigation
+//// Better way to use sticky navigation is through an api instead of the code below
+
+// const initialCoords = section1.getBoundingClientRect();
+// console.log(initialCoords);
+
+// window.addEventListener('scroll', function() {
+//   console.log(window.scrollY);
+
+//   if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+//   else nav.classList.remove('sticky');
+// })
+
+// Sticky Navigation: Intersection Observer API
+// const obsCallback = function(entries, observer) {
+//   entries.forEach(entry => {
+//     console.log(entry)
+//   })
+// }
+
+// const obsOptions = {
+//   root: null,
+//   threshold: [0, 0.2]
+// };
+
+// const observer = new IntersectionObserver(obsCallback, obsOptions);
+// observer.observe(section1);
+
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+
+
+const stickyNav = function (entries) {
+  const [entry] = entries; // same as entries[0] getting the first element
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
 });
+
+headerObserver.observe(header);
